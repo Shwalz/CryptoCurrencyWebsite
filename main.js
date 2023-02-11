f57d306e61f7c310cb72d83fe34194b2502c39f7
Shwalz <yahorfalkouski@gmail.com>
2023-02-11 20:14:21 +0100

    CryptoCurrencyWebsite
    Web page.

---
+++ main.js
@@ -0,0 +1,20 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId)
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+// Remove menu
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
